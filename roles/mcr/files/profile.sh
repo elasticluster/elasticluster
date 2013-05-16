34bf8d91b265dac032d549dd349a039cd7a8cc74
Riccardo Murri <riccardo.murri@gmail.com>
2013-05-16 11:15:24 +0200

    New playbook to install the MATLAB Compiler Runtime.
    Note that the provided `MCRInstaller.zip` file is a
    stub one; you have to replace it with the real installer
    zipfile that's in a MATLAB installation.
    
      new file:   matlab_compiler_runtime.yml
      new file:   mcr/files/MCRInstaller.zip
      new file:   mcr/files/input.txt
      new file:   mcr/files/install
      new file:   mcr/files/profile.sh

---
+++ roles/mcr/files/profile.sh
@@ -0,0 +1,5 @@
+# This is suggested by the the MCR installer, which however does not
+# create the profile files itself...
+#
+LD_LIBRARY_PATH=/opt/MATLAB/MATLAB_Compiler_Runtime/v717/runtime/glnxa64:/opt/MATLAB/MATLAB_Compiler_Runtime/v717/bin/glnxa64:/opt/MATLAB/MATLAB_Compiler_Runtime/v717/sys/os/glnxa64:/opt/MATLAB/MATLAB_Compiler_Runtime/v717/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/opt/MATLAB/MATLAB_Compiler_Runtime/v717/sys/java/jre/glnxa64/jre/lib/amd64/server:/opt/MATLAB/MATLAB_Compiler_Runtime/v717/sys/java/jre/glnxa64/jre/lib/amd64:$LD_LIBRARY_PATH
+XAPPLRESDIR=/opt/MATLAB/MATLAB_Compiler_Runtime/v717/X11/app-defaults
