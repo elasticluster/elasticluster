5380b6d69f6297e78077394d221e511f88231427
Antonio Messina <antonio.s.messina@gmail.com>
2013-09-04 17:51:37 +0200

    Refactor the `Cluster.start` method to use a pool of threads (one thread per virtual machine right now)

--- elasticluster/cluster.py
+++ elasticluster/cluster.py
@@ -101,6 +101,31 @@ class Cluster(object):
             if self.nodes[node.type][index]:
                 del self.nodes[node.type][index]
 
+    @staticmethod
+    def _start_node(node):
+        """
+        Static method to start a specific node on a cloud
+        """
+        log.debug("_start_node: working on node %s" % node.name)
+        # TODO: the following check is not optimal yet. When a
+        # node is still in a starting state,
+        # it will start another node here,
+        # since the `is_alive` method will only check for
+        # running nodes (see issue #13)
+        if node.is_alive():
+            log.info("Not starting node %s which is "
+                     "already up&running.", node.name)
+            return True
+        else:
+            try:
+                node.start()
+                log.info("_start_node: node has been started")
+                return True
+            except Exception as e:
+                log.error("could not start node `%s` for reason "
+                          "`%s`" % (node.name, e))
+                return None
+
     def start(self):
         """
         Starts the cluster with the properties given in the
@@ -123,45 +148,13 @@ class Cluster(object):
             log.error("user interruption: saving cluster before exit.")
             self.keep_running = False
 
-        # start every node
-        def start_node(node_queue):
-            try:
-                while not node_queue.empty():
-                    if not self.keep_running:
-                        log.error("Aborting execution upon CTRL-C")
-                        break
-                    node = node_queue.get()
-                    # TODO: the following check is not optimal yet. When a
-                    # node is still in a starting state,
-                    # it will start another node here,
-                    # since the `is_alive` method will only check for
-                    # running nodes (see issue #13)
-                    if node.is_alive():
-                        log.info("Not starting node %s which is "
-                                 "already up&running.", node.name)
-                    else:
-                        log.info("starting node...")
-                        try:
-                            node.start()
-                        except (InstanceError, SecurityGroupError,
-                        KeypairError, ImageError) as e:
-                            log.error("could not start node `%s` for reason "
-                                      "`%s`" % (node.name, e))
-
-            except Empty:
-                # nothing to do if the queue turns out to be empty - the
-                # nodes are then already started.
-                pass
-
-        thread_pool = Pool(processes=1)
-        manager = Manager()
-        node_queue = manager.Queue()
-        for node in self.get_all_nodes():
-            node_queue.put(node)
-
+        nodes = self.get_all_nodes()
+        thread_pool = Pool(processes=len(nodes))
+        log.debug("Created pool of %d threads" % len(nodes))
         signal.signal(signal.SIGINT, sigint_handler)
 
-        result = thread_pool.apply_async(start_node, (node_queue,))
+        # This is blocking
+        result = thread_pool.map_async(self._start_node, nodes)
 
         while not result.ready():
             result.wait(1)
