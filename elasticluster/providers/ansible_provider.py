40a06ce04983574996525db79d2985ff21ed3759
Antonio Messina <antonio.s.messina@gmail.com>
2013-12-14 21:53:33 +0100

    Add myself to the list of authors

--- elasticluster/providers/ansible_provider.py
+++ elasticluster/providers/ansible_provider.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-__author__ = 'Nicolas Baer <nicolas.baer@uzh.ch>'
+__author__ = 'Nicolas Baer <nicolas.baer@uzh.ch>, Antonio Messina <antonio.s.messina@gmail.com>'
 
 # system imports
 import logging
