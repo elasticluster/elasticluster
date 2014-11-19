0eb95fe221e2bae920447db500d9a6aaac13e940
Eduardo Santamaria <edsanav@gmail.com>
2014-11-19 16:55:27 +0100

    Checking locks

--- elasticluster/providers/openstack.py
+++ elasticluster/providers/openstack.py
@@ -249,7 +249,7 @@ class OpenStackCloudProvider(AbstractCloudProvider):
                         name, public_key_path, self._os_auth_url)
                     raise KeypairError(
                         "could not create keypair `%s`: %s" % (name, ex))
-        print "lock2: another lock"
+        print "lock2: another rlock"
         self._add_key_to_sshagent(private_key_path)
         lock.release()
         """
