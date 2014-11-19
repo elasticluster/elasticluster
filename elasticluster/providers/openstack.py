270c423794277f08ebc4422c9fbb4e1def3bcd9e
Eduardo Santamaria <edsanav@gmail.com>
2014-11-19 17:57:31 +0100

    Checking locks

--- elasticluster/providers/openstack.py
+++ elasticluster/providers/openstack.py
@@ -230,7 +230,7 @@ class OpenStackCloudProvider(AbstractCloudProvider):
         
         # Check if a keypair `name` exists on the cloud.
         
-        with OpenStackCloudProvider.__node_start_lock as fd:
+        with OpenStackCloudProvider.__node_start_rlock.acquire() as fd:
             try:
                 print "IT WAS LOCKEDDDDD"
                 keypair = self.client.keypairs.get(name)
