029c221985a84c5b8dbe3d75b167ed08ebc9dfd8
Eduardo Santamaria <edsanav@gmail.com>
2014-11-19 15:59:54 +0100

    Added function to check if fingerprint in ssh-agent

--- elasticluster/providers/openstack.py
+++ elasticluster/providers/openstack.py
@@ -247,6 +247,7 @@ class OpenStackCloudProvider(AbstractCloudProvider):
                         name, public_key_path, self._os_auth_url)
                     raise KeypairError(
                         "could not create keypair `%s`: %s" % (name, ex))
+        print "POST LOCKKKK!"
         self._add_key_to_sshagent(private_key_path)
         """
         if 'SSH_AUTH_SOCK' in os.environ.keys():
