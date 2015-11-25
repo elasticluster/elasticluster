eccbce3aa784feade8c1a13b73cf1a336f4a38a7
mbookman@google.com <mbookman@google.com>
2015-11-25 08:56:42 -0800

    Removed superfluous log.error()

--- elasticluster/providers/gce.py
+++ elasticluster/providers/gce.py
@@ -281,7 +281,6 @@ class GoogleCloudProvider(AbstractCloudProvider):
               'preemptible': True
             }
           else:
-            log.error("Unknown scheduling option: '%s'" % scheduling)
             raise InstanceError("Unknown scheduling option: '%s'" % scheduling)
 
         # construct the request body
