af357c053e5df5788216d8315e5e6dbbe5591ebf
Adam Huffman <bloch@verdurin.com>
2016-03-26 09:37:02 +0000

    Use Ansible >= 2.0.0 with develop to obviate need for sudo fix on CentOS 7

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ def ansible_pb_files():
 required_packages = [
     'PyCLI',
     'paramiko',
-    'ansible>=1.9.4, <2.0.0',
+    'ansible>=2.0.0',
     'voluptuous>=0.8.2',
     'configobj',
     'coloredlogs',
