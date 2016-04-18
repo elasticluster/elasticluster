8e7cf818a34116bb05d0c0d35e2c27bccb996603
Riccardo Murri <riccardo.murri@gmail.com>
2016-04-18 17:48:34 +0200

    Install PDSH on all nodes by default.

---
+++ elasticluster/providers/ansible-playbooks/roles/pdsh/files/etc/profile.d/pdsh.sh
@@ -0,0 +1,8 @@
+# THIS FILE IS CONTROLLED BY ELASTICLUSTER
+# local modifications will be overwritten
+# the next time `elasticluster setup` is run!
+#
+
+# PDSH defaults to using `rsh` for executing remote commands, but SSH
+# is actually a much better default choice...
+export PDSH_RCMD_TYPE='ssh'
