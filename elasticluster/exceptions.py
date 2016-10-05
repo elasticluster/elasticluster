73b7bc30b3661bedb857f7020e5d066284654009
Riccardo Murri <riccardo.murri@gmail.com>
2016-10-05 22:04:26 +0200

    New exception `InstanceNotFoundError`.
    Start being more specific on the error conditions signalled by backends,
    so the CLI can react more intelligently.

--- elasticluster/exceptions.py
+++ elasticluster/exceptions.py
@@ -15,7 +15,10 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-__author__ = 'Nicolas Baer <nicolas.baer@uzh.ch>'
+__author__ = '''
+    Nicolas Baer <nicolas.baer@uzh.ch>,
+    Riccardo Murri <riccardo.murri@gmail.com>
+'''
 
 
 class ConfigurationError(Exception):
@@ -39,8 +42,21 @@ class KeypairError(Exception):
 
 
 class InstanceError(Exception):
+    """
+    Generic error dealing with cloud-based VM.
+
+    The difference between this and other errors raised by cloud APIs (e.g.,
+    `FlavorError`:class:) is that `InstanceError` occurs when operations fail
+    on a VM instance that ElastiCluster assumes *existing* (whereas, e.g.,
+    `FlavorError`:class: might happen when starting a VM instance fails).
+    """
     pass
 
+class InstanceNotFoundError(InstanceError):
+    """
+    The cloud provider does not know about the given VM instance.
+    """
+    pass
 
 class FlavorError(Exception):
     pass
