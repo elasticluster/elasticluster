cba52914fd9763321a62a0b384846575db19519c
Riccardo Murri <riccardo.murri@gmail.com>
2017-04-25 15:35:50 +0200

    Cosmetic changes.

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -15,6 +15,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+
 # pylint: disable=missing-docstring,invalid-name
 
 from __future__ import (absolute_import, division, print_function)
