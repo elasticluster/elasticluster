f21963ae7821a7398654fc384f1955b48ce21fc9
Riccardo Murri <riccardo.murri@gmail.com>
2018-01-11 22:48:35 +0100

    Make libcloud backend work again.
    The code in this commit is able to start and stop a cluster on
    OpenStack using the `libcloud` provider with `openstack` driver.
    
    Unfortunately, despite the project's goal and documentation, it seems
    that base API calls are not uniformly supported across all the
    drivers: for instance, the `azure` driver lacks SSH key management
    methods, which would be part of libcloud's public API since version
    0.14.0 (see <https://libcloud.readthedocs.io/en/latest/compute/key_pair_management.html>)
    
    Therefore, it is difficult to tell on what providers the code would
    actually work, without testing them one by one...

--- elasticluster/providers/libcloud_provider.py
+++ elasticluster/providers/libcloud_provider.py
@@ -71,51 +71,72 @@ class LibCloudProvider(AbstractCloudProvider):
 
     def __init__(self, driver_name, storage_path=None, **options):
         self.storage_path = storage_path
-        args = None
-        if self.provider_args.has_key(driver_name.lower()):
-            log.debug('provider {0} requires arguments'.format(driver_name))
-            req_args = self.provider_args[driver_name.lower()]
-            if not set(req_args).issubset(options):
-                raise ValueError('{0} provider requires {1} to be set'.format(
-                    driver_name, ' '.join(req_args)
-                ))
-            args = ()
-            for r in req_args:
-                args += (options.pop(r),)
+        driver_name = driver_name.lower()
         try:
-            driver_name = getattr(Provider, driver_name.upper())
-        except AttributeError:
-            raise ValueError("No libcloud driver for provider {name}".format(name=driver_name))
+            req_args = self.provider_args[driver_name]
+            if not set(req_args).issubset(options):
+                raise ValueError(
+                    'Cloud provider {0} requires all of {1} to be set'
+                    .format(driver_name, ' '.join(req_args)))
+            args = [options.pop(name) for name in req_args]
+        except KeyError:
+            # no required args?!
+            args = []
         # fix for openstack
         if 'auth_url' in options and 'ex_force_auth_url' not in options:
             options['ex_force_auth_url'] = options['auth_url'].rsplit('/', 1)[0]
-        driver_class = get_driver(driver_name)
-        log.debug('Initializing libcloud driver `%s` ...', driver_class.__name__)
-        if args:
-            self.driver = driver_class(*args, **options)
-        else:
-            self.driver = driver_class(**options)
+
+        try:
+            provider_name = getattr(Provider, driver_name.upper())
+        except AttributeError:
+            raise ValueError(
+                "No libcloud driver for provider {name}"
+                .format(name=driver_name))
+        driver_class = get_driver(provider_name)
+        log.debug(
+            "Initializing libcloud driver `%s` ...",
+            driver_class.__name__)
+        self.driver = driver_class(*args, **options)
 
     def __get_instance(self, instance_id):
         for node in self.driver.list_nodes():
             if node.id == instance_id:
                 return node
-        return None
+        else:
+            log.warn('could not find instance with id %s', instance_id)
+            return None
 
-    def start_instance(self, key_name, public_key_path, private_key_path, security_group, flavor, image_id,
-                       image_userdata, username=None, node_name=None, **options):
+    def start_instance(self, key_name, public_key_path, private_key_path,
+                       security_group, flavor, image_id, image_userdata,
+                       username=None, node_name=None, **options):
         self.__prepare_key_pair(key_name,
                                 private_key_path,
                                 public_key_path,
                                 options.get('image_user_password'))
         options['name'] = node_name
-        options['size'] = flavor
-        options['image'] = image_id
+        options['size'] = self._get_flavor_by_name(flavor)
+        options['image'] = self.driver.get_image(image_id)
         if security_group:
             options['security_groups'] = security_group
         options['ex_userdata'] = image_userdata
         options['username'] = username
-        options['networks'] = options.pop('network_ids', None)
+
+        network_ids = [
+            netid.strip()
+            for netid in options.pop('network_ids', '').split(',')
+            if netid.strip() != ''
+        ]
+        if network_ids:
+            try:
+                options['networks'] = [
+                    net for net in self.driver.ex_list_networks()
+                    if net.id in network_ids
+                ]
+            except AttributeError:
+                raise UnsupportedError(
+                    "Cluster specifies `network_ids`"
+                    " but the cloud provider does not implement"
+                    " the `ex_list_networks()` call.")
 
         if self.driver.get_key_pair(key_name):
             options['auth'] = NodeAuthSSHKey(self.driver.get_key_pair(key_name).public_key)
@@ -123,35 +144,36 @@ class LibCloudProvider(AbstractCloudProvider):
         else:
             options['auth'] = NodeAuthPassword(options.get('image_user_password'))
 
-        for key in options.keys():
-            try:
-                list_fn = self.__get_function_by_pattern('list_{0}'.format(key))
-            except AttributeError:
-                # skip non-existing
-                continue
-            populated_list = self.__get_name_or_id(options[key], list_fn())
-            if populated_list:
-                if key.endswith('s'):
-                    options[key] = populated_list
-                else:
-                    options[key] = populated_list[0]
-
         node = self.driver.create_node(**options)
         if node:
             return node.id
         return None
 
+    def _get_flavor_by_name(self, name):
+        flavors = [
+            flavor for flavor in self.driver.list_sizes()
+            if (flavor.name == name or flavor.id == name)
+        ]
+        if flavors:
+            flavor = flavors[0]
+            if len(flavors) > 1:
+                log.warn(
+                    "%d flavors with name '%s' found!"
+                    " using first returned one: %s",
+                    len(flavors), flavor)
+            return flavor
+        else:
+            raise FlavorError("Cannot find flavor `%s`" % name)
+
     def is_instance_running(self, instance_id):
         instance = self.__get_instance(instance_id)
         if not instance:
-            log.warn('could not find instance with id %s', instance_id)
             return False
         return instance.state == NodeState.RUNNING
 
     def stop_instance(self, instance_id):
         instance = self.__get_instance(instance_id)
         if not instance:
-            log.warn('could not find instance with id %s', instance_id)
             return
         log.info('stopping %s', instance.name)
         instance.destroy()
@@ -159,50 +181,40 @@ class LibCloudProvider(AbstractCloudProvider):
     def get_ips(self, instance_id):
         instance = self.__get_instance(instance_id)
         if not instance:
-            log.warn('could not find instance with id %s', instance_id)
             return []
         return instance.public_ips + instance.private_ips
 
     def __prepare_key_pair(self, key_name, private_key_path, public_key_path, password):
         if not key_name:
-            log.warn('user_key_name has not been defined, assuming password based authentication')
+            log.warn('user_key_name has not been defined, assuming password-based authentication')
             return
 
-        try:
-            list_key_pairs = self.__get_function_by_pattern('list_key_pairs')
-        except AttributeError:
-            log.warn('key management not supported by provider, will try to continue without it...')
-            return
-
-        try:
-            self.__get_function_or_ex_function('import_key_pair_from_file')
-        except AttributeError:
-            raise UnsupportedError('key import not supported by provider')
-        try:
-            self.__get_function_or_ex_function('create_key_pair')
-        except AttributeError:
-            raise UnsupportedError('key creation not supported by provider')
-
-        if key_name in [k.name for k in list_key_pairs()]:
-            log.info('Key pair (%s) already exists, skipping import.', key_name)
+        if key_name in [k.name for k in self.driver.list_key_pairs()]:
+            log.info('Key pair `%s` already exists, skipping import.', key_name)
             return
 
         if public_key_path:
-            log.debug("importing public key from path %s", public_key_path)
-            key_import = self.__get_function_or_ex_function('import_key_pair_from_file')
-            if not key_import(name=key_name, key_file_path=os.path.expandvars(os.path.expanduser(public_key_path))):
-                raise KeypairError('failure during import of public key {p}'.format(p=public_key_path))
+            log.debug("importing public key from file %s ...", public_key_path)
+            if not self.driver.import_key_pair_from_file(
+                    name=key_name,
+                    key_file_path=os.path.expandvars(os.path.expanduser(public_key_path))):
+                raise KeypairError(
+                    'Could not upload public key {p}'
+                    .format(p=public_key_path))
         elif private_key_path:
             if not private_key_path.endswith('.pem'):
-                raise KeypairError('can only work with .pem private keys, derive public key and set user_key_public')
+                raise KeypairError(
+                    'can only work with .pem private keys,'
+                    ' derive public key and set user_key_public')
             log.debug("deriving and importing public key from private key")
             self.__import_pem(key_name, private_key_path, password)
-        elif os.path.exists(os.path.join(self.storage_path, '{p}.pem'.format(p=key_name))):
-            self.__import_pem(key_name, os.path.join(self.storage_path, '{}.pem'.format(key_name)), password)
         else:
-            with open(os.path.join(self.storage_path, '{p}.pem'.format(p=key_name)), 'w') as new_key_file:
-                new_key_file.write(self.__get_function_or_ex_function('create_key_pair')(name=key_name))
-            self.__import_pem(key_name, os.path.join(self.storage_path, '{p}.pem'.format(p=key_name)), password)
+            pem_file_path = os.path.join(self.storage_path, key_name + '.pem')
+            if not os.path.exists(pem_file_path):
+                with open(pem_file_path, 'w') as new_key_file:
+                    new_key_file.write(
+                        self.driver.create_key_pair(name=key_name))
+            self.__import_pem(key_name, pem_file_path, password)
 
     def __import_pem(self, key_name, pem_file_path, password):
         """
@@ -211,7 +223,6 @@ class LibCloudProvider(AbstractCloudProvider):
         :param pem_file_path: path to the pem file
         :param password: optional password for the pem file
         """
-        key_import = self.__get_function_or_ex_function('import_key_pair_from_file')
         pem_file = os.path.expandvars(os.path.expanduser(pem_file_path))
         try:
             pem = paramiko.RSAKey.from_private_key_file(pem_file, password)
@@ -226,49 +237,8 @@ class LibCloudProvider(AbstractCloudProvider):
         else:
             with NamedTemporaryFile('w+t') as f:
                 f.write('{n} {p}'.format(n=pem.get_name(), p=pem.get_base64()))
-                key_import(name=key_name, key_file_path=f.name)
-
-    def __get_function_by_pattern(self, pattern):
-        """
-        Return first function whose name *contains* the string `pattern`.
-
-        :param func: partial function name (ex. key_pair)
-        :return: list function that goes with it (ex. list_key_pairs)
-        """
-        function_names = [name for name in dir(self.driver) if pattern in name]
-        if function_names:
-            name = function_names[0]
-            if len(function_names) > 1:
-                log.warn(
-                    "Several functions match pattern `%s`: %r -- using first one!",
-                    pattern, function_names)
-            return getattr(self.driver, name)
-        else:
-            # no such function
-            raise AttributeError(
-                "No function name contains `{0}` in class `{1}`"
-                .format(pattern, self.__class__.__name__))
-
-    def __get_function_or_ex_function(self, func_name):
-        """
-        Check if a function (or an 'extended' function) exists for a key on a driver, and if it does, return it.
-        :param func_name: name of the function
-        :return: a callable or none
-        """
-        # try function name as given
-        try:
-            return getattr(self.driver, func_name)
-        except AttributeError:
-            pass
-        # try prefixing name with `ex_`
-        try:
-            return getattr(self.driver, 'ex_' + func_name)
-        except AttributeError:
-            pass
-        # no such function
-        raise AttributeError(
-            "No function named `{0}` or `{1}` in class `{2}`"
-            .format(func_name, 'ex_'+func_name, self.__class__.__name__))
+                self.driver.import_key_pair_from_file(
+                    name=key_name, key_file_path=f.name)
 
     @staticmethod
     def __get_name_or_id(values, known):
