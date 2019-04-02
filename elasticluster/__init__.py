1dd0dc861e899a8ea2d648c7c860b4e92eae17c7
Riccardo Murri <riccardo.murri@gmail.com>
2019-04-02 15:39:18 +0200

    Bump version: 1.3.dev8 → 1.3.dev9

--- elasticluster/__init__.py
+++ elasticluster/__init__.py
@@ -16,7 +16,7 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 __author__ = 'Nicolas Baer <nicolas.baer@uzh.ch>'
-__version__ = '1.3.dev8'
+__version__ = '1.3.dev9'
 
 
 # stdlib imports
