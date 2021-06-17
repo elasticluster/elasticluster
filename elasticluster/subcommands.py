61c623c734d5d814f53b3f23b353e3453b91e1cf
Robert Chase <rerpc@channing.harvard.edu>
2021-06-17 01:28:01 -0400

    add subcommands

--- elasticluster/subcommands.py
+++ elasticluster/subcommands.py
@@ -210,11 +210,12 @@ class Start(AbstractCommand):
             except ConfigurationError as err:
                 log.error("Starting cluster %s: %s", cluster_template, err)
                 return
-        if cluster.hasattr(extra):
+        if hasattr(cluster,extra):
+            print("Cluster has attr extra")
             cluster.extra["labels"] = labels
         else:
             print("Cluster has no extra attr")
-            cluster.setattr("extra",{'labels':labels})
+            setattr(cluster, "extra",{'labels':labels})
         try:
             print("Starting cluster `{0}` with:".format(cluster.name))
             for cls in cluster.nodes:
